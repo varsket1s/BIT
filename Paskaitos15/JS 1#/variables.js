9c3eef418e3c713c35e52175001b06520ca7e75a
Rytis Želvys <rytis6161@gmail.com>
2020-04-30 13:40:16 +0300

    Update variables.js

---
+++ Paskaitos15/JS 1#/variables.js
@@ -0,0 +1,13 @@
+"use strict";
+const manoVardas= "Rytis";  //tekstas
+const akiuSkaicius= 2;      //skaicius, nereik kabuciu
+const mokaSkaityti= true;   //nereik kabuciu
+const mokaSkraidyti= false;   //true arba false
+
+
+console.log(manoVardas, akiuSkaicius, mokaSkaityti)
+
+
+const marytesPazymiai=[9, 5, 3, 7];  //netuscias sarasas
+
+console.log(marytesPazymiai);
